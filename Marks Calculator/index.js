6f57fd7b3f1379d7a8e16e61b62a49093a832499
Aakash260 <71215606+Aakash260@users.noreply.github.com>
2023-07-14 09:17:36 +0530

    Marks-calculator

---
+++ Marks Calculator/index.js
@@ -0,0 +1,18 @@
+const formdata=document.getElementById('form');
+
+const calculateMarks=(event) => {
+    event.preventDefault(); 
+    const max=400;
+  const Data=new FormData(formdata);
+  const oData={};
+  Data.forEach((value,key)=>{
+      oData[key]= +value;
+    });
+    const Omaxm=oData.math +oData.english+oData.hindi+oData.science;
+    const per=(Omaxm/max)*100;
+     
+    const resEl=document.createElement('p');
+    resEl.className='result'
+    resEl.innerText=`You got ${Omaxm} out of 400 and Your Percentage is ${per}`
+    formdata.after(resEl)
+}
